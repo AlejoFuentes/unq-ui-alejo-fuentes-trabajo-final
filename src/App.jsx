7305bb2095548835f48f2743280c1ad747f8aa4e
AlejoFuentes <alejo.fuentess04@gmail.com>
2026-07-13 19:40:00 -0300

    agrego pantalla de inicio

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router';
+import StartGame from './components/StartGame';
+import Game from './components/Game';
+import EndGame from './components/EndGame';
 
 const App = () => {
   return (
--- src/components/StartGame.jsx
+++ src/components/StartGame.jsx
@@ -1,9 +1,22 @@
 import './StartGame.css';
+import { useNavigate } from 'react-router';
 
 const StartGame = () => {
+  
+  const navigate = useNavigate();
+
   return (
-    <div className="start-game-container"> 
-        Inicio del juego
+    <div className='h-100 align-items-center justify-content-center d-flex flex-column'>
+      <h1 className='mb-3'>Palabras Encadenadas</h1>
+      <div className='d-flex text-center'>
+        <p>Formá la cadena de palabras más larga posible antes de que se acabe el tiempo! <br />
+          Tenes 15 segundos para escribir una palabra que empiece con la última letra de la palabra anterior. <br />
+        </p>
+      </div>
+      <h2 className='mt-3'>¿Estás listo para jugar?</h2>
+      <button className="btn btn-primary mt-2" onClick={() => navigate('/game')}>
+        Comenzar Partida
+      </button>
     </div>
   );
 }
